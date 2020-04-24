c9ebc58333b147b37930f14e333edc6177835b9f
matheusquintaes <matheusquintaes@LAPTOP-L5I3F2FI.localdomain>
2020-04-24 13:11:52 +1200

    Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Header from "../Header"
+import GlobalStyles from "../../styles/global"
+
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header/>
+      <GlobalStyles/>
+      {children}
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
