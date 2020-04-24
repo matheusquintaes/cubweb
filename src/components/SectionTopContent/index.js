531ce1bdb428bcabe5f9254c139f20216d9d31a9
matheusquintaes <matheusquintaes@LAPTOP-L5I3F2FI.localdomain>
2020-04-24 13:12:09 +1200

    SectionTopContent

---
+++ src/components/SectionTopContent/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import * as S from "./styled"
+
+const SectionTopContent = ({
+  title,
+  description,
+}) => (
+  <S.SectionTopContent>
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </S.SectionTopContent>
+)
+
+SectionTopContent.propTypes = {
+  text: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+export default SectionTopContent 
---
+++ src/components/SectionTopContent/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const SectionTopContent = styled.div`
+
+  display:flex;
+  flex-direction: column;
+  text-align: center;
+
+   h1 {
+     color: var(--title-color);
+     font-size: 54px;
+     margin: 0 0 32px 0;
+   }
+  p{
+     max-width: 760px;
+     margin: 0 auto 32px auto;
+  }
+
+`
